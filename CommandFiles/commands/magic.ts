b602ffcdb67e65b125e6ef667512cdb889f9e70b
4voldy-anos <voldigoanos354@gmail.com>
2025-12-21 21:44:35 +0000

    Update magic.ts

--- CommandFiles/commands/magic.ts
+++ CommandFiles/commands/magic.ts
@@ -14,7 +14,7 @@ export const meta: CommandMeta = {
   description: "Generate AI images using Magic API",
   category: "AI",
   usage: "{prefix}{name} <prompt>",
-  role: 0,
+  role: 2,
   waitingTime: 5,
   icon: "🪄",
   noLevelUI: true,
